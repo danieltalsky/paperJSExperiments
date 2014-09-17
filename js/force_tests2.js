25c17d8cd38e0e78fd60f12c05a8ea95eef3bae4
Jonathan Harford <jonathan.harford@gmail.com>
2014-09-16 23:27:18 -0400

    Whoa we have springiness between the nodes and their parents!

--- js/force_tests2.js
+++ js/force_tests2.js
@@ -1,21 +1,17 @@
-var numNodes = 10;
-
 var TGStrokeColor = '#98c8eb';
 var TGOpacity = 0.5;
 var TGStrokeWidth = 3;
-var depthDistance = 40;
+var depthDistance = 80;
 
-var dampness = 0.9; // For dampening, of course.
-var rootForceCoeff = 0.01;
-var cousinForceCoeff = 0.0001;
-var parentForceCoeff = 0.000;
+var dampness = 0.95; // For dampening, of course.
+var stiffness = 6.0;
 
 var nodes = [];
 var parents = [
     -1, 0, 0, 0, 3, 4, 5, 4, 2, 9
 ];
 
-for (var i = 0; i < numNodes; i++) {
+for (var i = 0; i < parents.length; i++) {
     nodes.push({
         path: new Path.Circle({
             center: new Point(Math.random() * view.size.width,
@@ -24,13 +20,16 @@ for (var i = 0; i < numNodes; i++) {
             strokeColor: TGStrokeColor,
             strokeWidth: TGStrokeWidth,
             opacity: TGOpacity}),
-        m: 100,
-        v: new Point(6 * Math.random()-3,
-                     6 * Math.random()-3),
+        m: 10,
+        v: new Point(0.0, 0.0),
         a: new Point(0.0, 0.0),
     });
+    if (parents[i] == -1) {
+        nodes[i].generation = 0;
+    }
     if (parents[i] != -1) {
         nodes[i].parent = nodes[parents[i]];
+        nodes[i].generation = nodes[i].parent.generation + 1;
         nodes[i].parentBranch = new Path.Line({
             from: nodes[i].path.position, 
             to: nodes[i].parent.path.position,
@@ -40,53 +39,55 @@ for (var i = 0; i < numNodes; i++) {
     }
 }
 
-function accelNode(node) {
-    
+console.log(nodes);
+
+function getForce(n1, n2, k, d, b) {
+    // F = -k(|x|-d)(x/|x|) - bv
+    // k: spring tightness coeff
+    // d: desired distance of separation
+    // x: distance
+    // b: dampening coeff
+    // |x|: distance between the two points connected to the spring
+    // x/|x|: unit length direction vector between the two points: a to b, when applying the force to point a and vice versa.
+    // v: relative velocity between the two points connected by the spring
+    // Vector2 F1 = -k * (xAbs - d) * (Vector2.Normalize(node2.p - node1.p) / xAbs) - b * (node1.v - node2.v);
+
+    var x = n1.path.position - n2.path.position;
+    var xAbs = n1.path.position.getDistance(n2.path.position);
+    var v = n1.v - n2.v;
+    return (x / xAbs) * (-k) * (xAbs - d) - (v * b);
 }
 
-function moveNode(node) {
-    accelNode(node);
-    node.path.position += node.v;
+function branchAccel(node, k, d, b) {
+
+    node.a += getForce(node, node.parent, k, d, b)/node.m;
+    node.parent.a += getForce(node.parent, node, k, d, b)/node.parent.m;
 }
+
+function originAccel(node, k, d, b) {
+    node.a += getForce(node, nodes[0], k, node.generation * d, b)/node.m;
+    nodes[0].a += getForce(nodes[0], node, k, node.generation * d, b)/nodes[0].m;
+}    
 
 function onFrame(event) {
+    //console.log(nodes[0].path.position);
+    //console.log(nodes[0].v);
+    //console.log(nodes[0].a); 
     for (var i = 0; i < nodes.length; i++) {
-            //parentForce(generationsArray[i][j]);
-            moveNode(nodes[i]);
+        if ("parent" in nodes[i]) {
+            branchAccel(nodes[i], 1, depthDistance, dampness);
+            originAccel(nodes[i], 1, depthDistance, dampness);
+        }
     }
-}
+    for (var j = 0; j < nodes.length; j++) {
+        nodes[j].v += nodes[j].a;
+        nodes[j].a *= 0;
+        nodes[j].path.position += nodes[j].v;
+    }
+}       
 
-//    F = -k(|x|-d)(x/|x|) - bv
-//    k: spring tightness coeff
-//    d: desired distance of separation
-//    x: distance
-//    b: dampening coeff
-//    Vector2 F1 = -k * (xAbs - d) * (Vector2.Normalize(node2.p - node1.p) / xAbs) - b * (node1.v - node2.v);
-/*
-function parentForce(node) {
-    var k = 0.9;
-    var d = depthDistance;
-    var x = node.parent.path.position - node.path.position;
-    var b = 0.9;
-    node.velocity += -k * vec * parentForceCoeff;
-}
 
-function moveNode(node) {
-    node.path.position += node.velocity;
-    node.branch.to += node.velocity;  // WHY DOESN'T THIS WORK
-}
 
-function onFrame(event) {
-    for (var i = 0; i < generationsArray.length; i++) {
-        for (var j = 0; j <  generationsArray[i].length; j++) {
-            rootForce(generationsArray[i][j], i);
-            for (var k = j + 1; k < generationsArray[i].length; k++) {
-                var vec = generationsArray[i][j].path.position - generationsArray[i][k].path.position;
-                generationsArray[i][j].velocity += vec * cousinForceCoeff;
-            };
-            parentForce(generationsArray[i][j]);
-            moveNode(generationsArray[i][j]);
-        };
-    };
-}
-*/
+
+
+
